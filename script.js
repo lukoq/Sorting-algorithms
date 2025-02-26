15b31039202f103f938a7b4a8c12281ef46e4fd2
lukoq <kinderlukasz01@gmail.com>
2025-02-26 12:53:04 +0100

    style

--- script.js
+++ script.js
@@ -4,26 +4,30 @@ const array = [];
 const numBars = 64;
 const barWidth = canvas.width / numBars;
 
-for (let i = 0; i < numBars; i++) {
+for(let i = 0; i < numBars; i++){
     array.push(Math.floor(Math.random() * canvas.height));
 }
 
-function drawArray(arr) {
+function drawArray(arr, indx){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < arr.length; i++) {
-        ctx.fillStyle = "blue";
+    for(let i = 0; i < arr.length; i++) {
+        ctx.fillStyle = "#89cff0";
+        if(i === indx){
+            ctx.fillStyle = "#f0899c";
+        }
         ctx.fillRect(i * barWidth, canvas.height - arr[i], barWidth - 2, arr[i]);
     }
 }
 
-async function bubbleSort() {
+async function bubbleSort(){
     let len = array.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                [array[j], array[j + 1]] = [array[j + 1], array[j]]; // Swap
-                drawArray(array);
-                await new Promise(resolve => setTimeout(resolve, 10)); // Opóźnienie animacji
+    for(let i = 0; i < len; i++){
+        for(let j = 0; j < len - i - 1; j++) {
+            if(array[j] > array[j + 1]){
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+
+                drawArray(array, j+1);
+                await new Promise(resolve => setTimeout(resolve, 50));
             }
         }
     }
@@ -34,3 +38,4 @@ function startSorting() {
 }
 
 drawArray(array);
+
